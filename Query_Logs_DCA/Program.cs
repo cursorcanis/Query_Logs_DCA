4ff5af9149123dc57e2f952dec45dd5060ee2df7
Alfredo Alea <alfredoalea@gmail.com>
2023-02-16 16:25:52 -0500

    added some information to the connection string to Azure in Program.cs

--- Query_Logs_DCA/Program.cs
+++ Query_Logs_DCA/Program.cs
@@ -3,10 +3,18 @@ using Microsoft.Extensions.DependencyInjection;
 using Query_Logs_DCA.Data;
 var builder = WebApplication.CreateBuilder(args);
 
+
+string connectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+
+//builder.Configuration.AddAzureAppConfiguration(connectionString);
+
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Query_Logs_DCAContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING") ?? throw new InvalidOperationException("Connection string 'AZURE_SQL_CONNECTIONSTRING' not found.")));
+
+
 
 var app = builder.Build();
 
