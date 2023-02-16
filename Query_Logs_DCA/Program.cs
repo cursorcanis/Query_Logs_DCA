d2a166145ff1fe0cc48ca4c83bb9b5fff1c5a311
Alfredo Alea <alfredoalea@gmail.com>
2023-02-16 13:52:19 -0500

    Erased the Default: information portion of the appsettings.json file.

--- Query_Logs_DCA/Program.cs
+++ Query_Logs_DCA/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Query_Logs_DCAContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING") ?? throw new InvalidOperationException("Connection string 'Query_Logs_DCAContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING") ?? throw new InvalidOperationException("Connection string 'AZURE_SQL_CONNECTIONSTRING' not found.")));
 
 var app = builder.Build();
 
