e445232081cc42e53312f07a86c3ae4d1d2ffaad
Alfredo Alea <alfredoalea@gmail.com>
2023-02-20 12:04:13 -0500

    Updated the SeedData for the addition of the Rating field in the previous update

--- Query_Logs_DCA/Models/SeedData.cs
+++ Query_Logs_DCA/Models/SeedData.cs
@@ -25,33 +25,37 @@ namespace Query_Logs_DCA.Models
                 context.Query.AddRange(
                     new Query
                     {
-                        Title_of_Query = "When Harry Met Sally",
+                        Title_of_Query = "When Users cannot be located",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
-                        Category_of_Query = "Romantic Comedy",
+                        Category_of_Query = "Searching for fields",
+                        Rating_Complexity = "Moderate"
                         //Price = 7.99M
                     },
 
                     new Query
                     {
-                        Title_of_Query = "Ghostbusters ",
+                        Title_of_Query = "Ghost in the machines Query ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
-                        Category_of_Query = "Comedy",
+                        Category_of_Query = "Comedy of SQL Errors",
+                        Rating_Complexity = "High Complexity"
                         //Price = 8.99M
                     },
 
                     new Query
                     {
-                        Title_of_Query = "Ghostbusters 2",
+                        Title_of_Query = "Machine crusaders in fields",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
-                        Category_of_Query = "Comedy",
+                        Category_of_Query = "Schema updater",
+                        Rating_Complexity = "Low"
                         //Price = 9.99M
                     },
 
                     new Query
                     {
-                        Title_of_Query = "Rio Bravo",
+                        Title_of_Query = "Rio of Queries",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
-                        Category_of_Query = "Western",
+                        Category_of_Query = "Whole Schema",
+                        Rating_Complexity = "Medium difficulty"
                         //Price = 3.99M
                     }
                 );
