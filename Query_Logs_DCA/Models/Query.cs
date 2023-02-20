5158ecd2d177261aa1400c0c705ad1d7a6d5be27
Alfredo Alea <alfredoalea@gmail.com>
2023-02-19 23:10:06 -0500

    Erased part of the .yml file and changed the Query.cs file

--- Query_Logs_DCA/Models/Query.cs
+++ Query_Logs_DCA/Models/Query.cs
@@ -6,11 +6,14 @@ namespace Query_Logs_DCA.Models
     public class Query
     {
         public int Id { get; set; }
-        public string? Title_of_Query { get; set; }
+        public string? Title_of_Query { get; set; } = string.Empty;
+
+        [Display(Name = "Time Query was Released")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        public string? Category_of_Query { get; set; }
+        public string? Category_of_Query { get; set; } = string.Empty;
         //public decimal Price { get; set; }
 
     }
 }
+
